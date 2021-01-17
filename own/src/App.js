0c4cd7c7253f3f7e2799fee0f82db354451d3779
CHIA-YU CHEN <b06611016@ntu.edu.tw>
2021-01-18 01:35:51 +0800

    1/17 Andrew summit

--- own/src/App.js
+++ own/src/App.js
@@ -7,18 +7,9 @@ import { Button } from 'react-bootstrap';
 function App() {
   const [username, setUsername] = useState('')
   const [login, setLogin] = useState(false)
-  const [body, setBody] = useState('')
-  const [receiver, setReceiver] = useState('')
-
+  
   const bodyRef = useRef(null)
-  //const { sent, receive, status, opened, data, sendMessage, clearMessages, setsent, setreceive, refetch } = useChat()
-
-  //const [username, setUsername] = useState('')
-  //const [body, setBody] = useState('')
-  //const [enter, setEnter] = useState(false)
-
-  //const [time, setTime] = useState(0)
-  //const bodyRef = useRef(null)
+  
 
   const displayStatus = (s) => {
     if (s.msg) {
@@ -43,7 +34,68 @@ function App() {
     }
   }
 
-  /*const chat = (
+  const loginpage = (
+    <>
+      <div className="App-title">
+        <h1>Fitting App</h1>
+      </div>
+      <div className="App-body">
+        <div className="App-row">
+          <h1>Account:</h1>
+          <Input
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            style={{ marginBottom: 10 }}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                bodyRef.current.focus()
+              }
+            }}
+          ></Input>
+        </div>
+      
+        <div className="App-row">
+          <h1>Password:</h1>
+          <Input
+            rows={4}
+            value={receiver}
+            ref={bodyRef}
+            onChange={(e) => setReceiver(e.target.value)}
+            placeholder="Password"
+            onKeyDown={(e) => {
+              console.log("Key")
+            }}
+          ></Input>
+        </div>
+        <div className="App-row">
+          <Button variant="success" onClick={() => {console.log("登入")}}>
+            Log in
+          </Button>
+          <Button variant="primary" onClick={() => {console.log("創建新帳號")}}>
+            Create
+          </Button>
+        </div>
+      </div>
+    </>
+  )
+
+  return <div>{loginpage}</div>
+}
+
+export default App
+
+//const [body, setBody] = useState('')
+  //const [receiver, setReceiver] = useState('')
+
+  /*cons//const { sent, receive, status, opened, data, sendMessage, clearMessages, setsent, setreceive, refetch } = useChat()
+
+  //const [username, setUsername] = useState('')
+  //const [body, setBody] = useState('')
+  //const [enter, setEnter] = useState(false)
+
+  //const [time, setTime] = useState(0)
+  //const bodyRef = useRef(null)t chat = (
     <div className="App">
       <div className="App-title">
         <Button type="primary" onClick={() => {
@@ -133,53 +185,3 @@ function App() {
     displayStatus(status)
   }, [status])*/
   
-  const loginpage = (
-    <>
-      <div className="App-title">
-        <h1>Fitting App</h1>
-      </div>
-      <div className="App-body">
-        <div className="App-row">
-          <h1>Account:</h1>
-          <Input
-            placeholder="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            style={{ marginBottom: 10 }}
-            onKeyDown={(e) => {
-              if (e.key === 'Enter') {
-                bodyRef.current.focus()
-              }
-            }}
-          ></Input>
-        </div>
-      
-        <div className="App-row">
-          <h1>Password:</h1>
-          <Input
-            rows={4}
-            value={receiver}
-            ref={bodyRef}
-            onChange={(e) => setReceiver(e.target.value)}
-            placeholder="Password"
-            onKeyDown={(e) => {
-              console.log("Key")
-            }}
-          ></Input>
-        </div>
-        <div className="App-row">
-          <Button variant="success" onClick={() => {console.log("登入")}}>
-            Log in
-          </Button>
-          <Button variant="primary" onClick={() => {console.log("創建新帳號")}}>
-            Create
-          </Button>
-        </div>
-      </div>
-    </>
-  )
-
-  return <div>{loginpage}</div>
-}
-
-export default App
--- own/src/graphql/mutation.js
+++ own/src/graphql/mutation.js
@@ -1,39 +1,15 @@
 import { gql } from 'apollo-boost'
 
-export const CREATE_CHAT_MUTATION = gql`
-    mutation createChat(
-        $sent: String!
-        $receive: String!
-        $body: String!
+export const CREATE_USER_MUTATION = gql`
+    mutation createUser(
+        $username: String!
+        $password: String!
+        $strength: String!
     ){
-        createChat(
-            request: "input"
-            data: {
-                sent: $sent
-                receive: $receive
-                body: $body
-            }
-        ){
-            task
-            payload {
-                sent
-                receive
-                body
-            }
-        }
-    }
-`
-export const DELET_ALL_CHATS_MUTATION = gql`
-    mutation deleteChats(
-        $request: String!
-        $sent: String!
-    ){
-        deleteChats(
-            request: $request
-            sent: $sent
-        ){
-            type
-            msg
-        }
+        createUser(
+            username: $username
+            password: $password
+            strength: $strength
+        )
     }
 `
