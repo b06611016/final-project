798f5964c44799150fda441da3cb1e1a5027790f
eric <hsinyang0816@github.com>
2021-01-19 02:12:04 +0800

    1/19 Eric summit

---
+++ own/src/components/MenuPage.js
@@ -0,0 +1,34 @@
+import React, { useState, useRef, useEffect } from 'react'
+import { Button } from 'react-bootstrap'
+import { Input, message, Tag } from 'antd'
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar'
+import './MenuPage.css'
+
+const MenuPage = (props) => {
+    const { username, password } = props;
+    const contents = [10, 20, 30, 40, 50];
+
+    return (
+        <>
+            <div className="MenuPage-title">
+                <h1 id="h1">Let's workout!</h1>
+                <p>{"Strength level : " + "High intensity"}</p>
+            </div>
+            <div className="MenuPage-body">
+                {contents.map((e, index) => {
+                    return (
+                        <Button variant="secondary" key={index}>
+                            <p id="p">{"Day " + (index + 1)}</p>
+                            <CircularProgressbar id="cb" value={e} text={`${e}%`} styles={buildStyles({
+                                textColor: "gold",
+                                pathColor: "gold"
+                            })} />
+                        </Button>
+                    )
+                })}
+            </div>
+        </>
+    )
+}
+
+export default MenuPage;
