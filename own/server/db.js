787b29550e3462617594512167781790402ae4ab
CHIA-YU CHEN <b06611016@ntu.edu.tw>
2021-01-19 05:32:34 +0800

    1/19 Andrew submit

---
+++ own/server/db.js
@@ -0,0 +1,28 @@
+const low = [
+    {
+        exercise: ["push up", "jumping jack"],
+        sec: [5, 5]
+    }
+]
+
+const medium = [
+    {
+        exercise: ["push up", "jumping jack"],
+        sec: [10, 10]
+    }
+]
+
+const high = [
+    {
+        exercise: ["push up", "jumping jack"],
+        sec: [10, 10]
+    }
+]
+
+const data = {
+    low,
+    medium,
+    high
+}
+
+export { data as default }
