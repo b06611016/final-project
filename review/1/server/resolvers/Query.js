aa81367b89ab7cea422be5f529b578567f544b5c
CHIA-YU CHEN <b06611016@ntu.edu.tw>
2021-01-18 17:42:14 +0800

    1/18 Andrew summit

--- review/1/server/resolvers/Query.js
+++
@@ -1,8 +0,0 @@
-const Query = {
-    async messages(parent, args, { Message }, info){
-        const data = await Message.find().sort({ _id: 1 })
-        return data.filter(e => !args.user || e.sender === args.user || e.receiver === args.user)
-    }
-}
-
-module.exports = Query
--- review/1/src/graphql/index.js
+++
@@ -1,9 +0,0 @@
-import CLEAR_MESSAGES from './clearMessages.js'
-import SEND_MESSAGE from './sendMessage.js'
-import SUBSCRIBE from './subscribe.js'
-import QUERY from './messageQuery.js'
-
-export { CLEAR_MESSAGES }
-export { SEND_MESSAGE }
-export { SUBSCRIBE }
-export { QUERY }
