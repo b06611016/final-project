73ca6351a0f8674630359f1d0b4ccf16ececfbf9
CHIA-YU CHEN <b06611016@ntu.edu.tw>
2021-01-20 00:45:13 +0800

    1/20 Andrew submit

--- own/src/containers/App.js
+++ own/src/containers/App.js
@@ -1,16 +1,11 @@
 import './App.css'
 import React, { useEffect, useRef, useState } from 'react'
 import useLogin from './useLogin'
-import LoginPage from '../components/LoginPage'
-import FormPage from '../components/FormPage'
-import MenuPage from '../components/MenuPage'
-import DayPage from '../components/DayPage'
-import CountingPage from '../components/CountingPage'
-/*import { useQuery, useMutation } from '@apollo/react-hooks'
-import {
-  USER_QUERY,
-  CREATE_USER_MUTATION,
-} from '../graphql' */
+//import LoginPage from '../components/LoginPage'
+//import FormPage from '../components/FormPage'
+//import MenuPage from '../components/MenuPage'
+//import DayPage from '../components/DayPage'
+//import CountingPage from '../components/CountingPage'
 
 
 function App() {
@@ -25,7 +20,7 @@ function App() {
     variables: { username: username, password: password}
   })*/
   //const [addUser] = useMutation(CREATE_USER_MUTATION)
-  const { username, setUsername, login, setlogin, create, setCreate, password, setPassword, strength, setstrength, gender, setgender, createaccount, setcreate } = useLogin();
+  const { loginpage, login, create, formpage, menupage } = useLogin();
 
   //---------------------------------------------------------------------------------------------Function----------------------------------------------------------------------------
   /*const checklogin = () => {
@@ -40,15 +35,8 @@ function App() {
 
 
   //-----------------------------------------------------------------------------------------------Page------------------------------------------------------------------------------
-  const loginpage = (
-    <LoginPage onChange1={(e) => setUsername(e.target.value)} onChange2={(e) => setPassword(e.target.value)} onClick1={setlogin} onClick2={setcreate} username={username} password={password}></LoginPage>
-  )
 
-  const formpage = (
-    <FormPage username={username} password={password} onChange1={(e) => setUsername(e.target.value)} onChange2={(e) => setPassword(e.target.value)} onChange3={(e) => { setstrength(e) }} onChange4={(e) => { setgender(e) }} onClick1={createaccount}></FormPage>
-  )
-
-  const menupage = (
+  /*const menupage = (
     <MenuPage username={username} password={password}></MenuPage>
   )
 
@@ -58,10 +46,10 @@ function App() {
 
   const countingpage = (
     <CountingPage username={username} password={password}></CountingPage>
-  )
+  )*/
   //-----------------------------------------------------------------------------------------------Page------------------------------------------------------------------------------
   //return <div>{login ? menupage : (create ? formpage : loginpage)}</div>
-  return <div>{daypage}</div>
+  return <div>{login? menupage() : (create? formpage: loginpage)}</div>
 }
 
 export default App
---
+++ own/src/containers/useMenu.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect, useCallback } from 'react'
+import { useQuery } from '@apollo/react-hooks'
+import { EXERCISES_QUERY } from '../graphql'
+import { message } from 'antd'
+import MenuPage from '../components/MenuPage'
+import DayPage from '../components/DayPage'
+
+const useMenu = () => {
+    const [username, setUsername] = useState('');
+    const [completion, setCompletion] = useState(0);
+    const [strength, setStrength] = useState('');
+    const [login, setLogin] = useState(false);
+    const [days, setDays] = useState(0);
+    const [day, setDay] = useState(0);
+
+    const { loading, data, refetch } = useQuery(EXERCISES_QUERY, {
+        variables: { strength: strength }
+    });
+
+    const fetchExerciseSchedule = useCallback(() => {
+        if (login)
+            refetch();
+    }, [login])
+
+    useEffect(() => {
+        fetchExerciseSchedule();
+        console.log(data)
+        if (data){
+            setDays(data.queryExercises.length);
+            console.log(data.queryExercises)
+        }
+    }, [fetchExerciseSchedule, data]);
+
+    const menupage = () => {
+        if (day === 0)
+            return <MenuPage username={username} days={days} strength={strength} completion={completion} onclick={(e)=>{console.log(e)}}/>
+        else{
+            if(data)
+                return <DayPage exercises={data.queryExercises[day - 1].exercise} strength={strength} day={day}/>;
+        }
+    }
+    //console.log("day: " + day)
+    return { username, setUsername, completion, setCompletion, strength, setStrength, login, setLogin, menupage };
+}
+//setDay(parseInt(e.target.key) + 1)
+export default useMenu;
