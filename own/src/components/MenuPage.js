88ede13f7e2c2a55f9e74dace3bd0726087d138c
CHIA-YU CHEN <b06611016@ntu.edu.tw>
2021-01-20 00:45:47 +0800

    1/20 Andrew submit

--- own/src/components/MenuPage.js
+++ own/src/components/MenuPage.js
@@ -4,26 +4,48 @@ import { CircularProgressbar, buildStyles } from 'react-circular-progressbar'
 import './MenuPage.css'
 
 const MenuPage = (props) => {
-    const { username, password } = props;
-    const contents = [10, 20, 30, 40, 50];
+    const { username, days, strength, completion, onclick } = props;
+
+    let contents = [];
+    for (let i = 0; i < days; ++i){
+        if (i < completion)
+            contents.push(100);
+        else
+            contents.push(0);
+    }
+
+    console.log(completion);
+    console.log(days)
 
     return (
         <>
             <div className="MenuPage-title">
                 <h1 id="h1">Let's workout!</h1>
-                <p>{"Strength level : " + "High intensity"}</p>
+                <p>{"Strength level : " + strength + " intensity"}</p>
             </div>
             <div className="MenuPage-body">
                 {contents.map((e, index) => {
-                    return (
-                        <Button variant="secondary" key={index}>
-                            <p id="p">{"Day " + (index + 1)}</p>
-                            <CircularProgressbar id="cb" value={e} text={`${e}%`} styles={buildStyles({
-                                textColor: "gold",
-                                pathColor: "gold"
-                            })} />
-                        </Button>
-                    )
+                    console.log(index);
+                    if (index === completion)
+                        return (
+                            <Button variant="secondary" key={index} onClick={onclick}>
+                                <p id="p">{"Day " + (index + 1)}</p>
+                                <CircularProgressbar id="cb" value={e} text={`${e}%`} styles={buildStyles({
+                                    textColor: "gold",
+                                    pathColor: "gold"
+                                })} />
+                            </Button>
+                        );
+                    else
+                        return (
+                            <Button variant="secondary" key={index} onClick={onclick} disabled>
+                                <p id="p">{"Day " + (index + 1)}</p>
+                                <CircularProgressbar id="cb" value={e} text={`${e}%`} styles={buildStyles({
+                                    textColor: "gold",
+                                    pathColor: "gold"
+                                })} />
+                            </Button>
+                        );
                 })}
             </div>
         </>
